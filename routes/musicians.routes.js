f67cfcf8b32ebc4586d3c96b276bb7c9ef7b7c82
Wale <walekolade45@gmail.com>
2022-03-10 15:42:27 +0100

    musician route updated

--- routes/musicians.routes.js
+++ routes/musicians.routes.js
@@ -22,9 +22,12 @@ router.post('/edit/:id', fileUploader.single('image'), (req, res) => {
     const {id} = req.params
     const { username, instruments, mediaLinks, genres, description } = req.body
     console.log('post here',instruments,)
-    let newInstruments  = instruments.split(" ");
+    const newInstruments  = instruments.split(" ");
+    const newMediaLinks = mediaLinks.split(" ");
+    const newGenres = genres.split(" ");
+    const newDescription = description.split(" ");
 
-    Musician.findOneAndUpdate({_id: id}, { username, instruments :newInstruments, mediaLinks, genres, description,  imageUrl: req.file?.path })
+    Musician.findOneAndUpdate({_id: id}, { username, instruments :newInstruments, mediaLinks : newMediaLinks, genres : newGenres, description : newDescription, imageUrl: req.file?.path })
         .then(createdMusicainFrDB => {
             console.log(createdMusicainFrDB)
             res.redirect('/musicians/musicianProfile')
