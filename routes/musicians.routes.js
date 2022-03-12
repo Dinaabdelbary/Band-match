453a91f5dd39df77cd661611f2d05afe2ffb8666
Wale <walekolade45@gmail.com>
2022-03-12 15:28:10 +0100

    only one logout button

--- routes/musicians.routes.js
+++ routes/musicians.routes.js
@@ -2,13 +2,26 @@ const router = require("express").Router();
 const Musician = require("../models/Musician.model.js");
 const fileUploader = require("../config/cloudinary.config");
 
-router.get("/musicianProfile", (req, res) => {
-  Musician.findById(req.session.currentUser._id).then((musicianFrDB) => {
-      res.render("lists/musicianProfile.hbs", {
-        musician: musicianFrDB,
-      });
-  });
-});
+
+
+router.get('/profile/edit', (req, res) => {
+  const {currentUser} = req.session;
+  console.log(currentUser)
+
+  if(!currentUser){
+        res.redirect('/authview/login')
+   }
+
+   Musician.findById(req.session.currentUser._id)
+   .then((musicianFrDB) => {
+     console.log("musicianFrDB",musicianFrDB);
+    res.render("profile/edit-profile", {
+      musician: musicianFrDB,
+    })
+   
+}) .catch(error => console.log(error))
+
+})
 
 // router.get("/profile", (req, res) => {
 //     console.log(" i am here");
@@ -23,6 +36,17 @@ router.get("/musicianProfile", (req, res) => {
 //   //     musician: req.session.currentUser
 //   // });
 // });
+
+router.get("/profile/:id", (req, res) => {
+  const { id } = req.params
+  Musician.findById(id)
+  .then((musicianFrDB) => {
+      res.render("profile/musicianProfile.hbs", {
+        musician: musicianFrDB,
+        isMyself: id === req.session.currentUser._id
+      });
+  }).catch(err => console.log(err));
+});
 
 router.post("/edit/:id", fileUploader.single("image"), (req, res) => {
   const { id } = req.params;
@@ -51,19 +75,20 @@ router.post("/edit/:id", fileUploader.single("image"), (req, res) => {
     .catch((error) => console.log(error));
 });
 
-router.post('/edit/:id', fileUploader.single('image'), (req, res) => {
-    const {id} = req.params
-    const { username, instruments, mediaLinks, genres, description } = req.body
-    console.log('post here',instruments,)
-    let newInstruments  = instruments.split(" ");
 
-    Musician.findOneAndUpdate({_id: id}, { username, instruments :newInstruments, mediaLinks, genres, description,  imageUrl: req.file?.path })
-        .then(createdMusicainFrDB => {
-            console.log(createdMusicainFrDB)
-            res.redirect('/musicians/musicianProfile')
-        })
-        .catch(error => console.log(error))
-})
+// router.post('/edit/:id', fileUploader.single('image'), (req, res) => {
+//     const {id} = req.params
+//     const { username, instruments, mediaLinks, genres, description } = req.body
+//     console.log('post here',instruments,)
+//     let newInstruments  = instruments.split(" ");
+
+//     Musician.findOneAndUpdate({_id: id}, { username, instruments :newInstruments, mediaLinks, genres, description,  imageUrl: req.file?.path })
+//         .then(createdMusicainFrDB => {
+//             console.log(createdMusicainFrDB)
+//             res.redirect('/musicians/musicianProfile')
+//         })
+//         .catch(error => console.log(error))
+// })
 
 router.get('/listofmusicians', (req, res) => {
 
