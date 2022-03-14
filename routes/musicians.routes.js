164e46e2de3edc3c0d77f8f0625dbee9a9661419
Wale <walekolade45@gmail.com>
2022-03-14 22:56:34 +0100

    Name dont show if its user profile

--- routes/musicians.routes.js
+++ routes/musicians.routes.js
@@ -96,5 +96,5 @@ router.get('/listofmusicians', (req, res) => {
 
     Musician.find()
     .then(musicians => res.render("authview/listofmusicians", {musicians}))
-})
+ })
 module.exports = router;
